81d36ad236ade744fdae05dd8f05d4e4ee709da1
dimas wicaksono <novan.top@gmail.com>
2020-06-25 23:36:07 +0700

    add install redux-persist and config for storage browse cart

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,18 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
 import userReducer from "./user/userReducer";
 import cartReduser from "./cart/cartReducer";
-export default combineReducers({
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReduser,
 });
+
+export default persistReducer(persistConfig, rootReducer);
