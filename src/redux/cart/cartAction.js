37705d6fb67ccd9b71ca5527f7979a2cb15b1161
dimas wicaksono <novan.top@gmail.com>
2020-06-25 13:42:26 +0700

    delete item on checkout page dispatch from checkout-item components with action clear item on reducer with filter function

--- src/redux/cart/cartAction.js
+++ src/redux/cart/cartAction.js
@@ -8,3 +8,8 @@ export const addItem = (item) => ({
   type: cartTypeAction.CART_ADD_ITEMS,
   payload: item,
 });
+
+export const clearItemFromCart = (item) => ({
+  type: cartTypeAction.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
