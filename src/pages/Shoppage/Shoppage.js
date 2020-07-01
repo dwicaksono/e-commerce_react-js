c0af59f867547971f009744ea3dce485ec0eee18
dimas wicaksono <novan.top@gmail.com>
2020-07-01 12:28:50 +0700

    add spinner loading component at collection page and collection overview

--- src/pages/Shoppage/Shoppage.js
+++ src/pages/Shoppage/Shoppage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import CollectionOverView from "../../components/collectionOverview/collectionOverview";
 import CollectionPage from "../Collection/CollectionPage";
+import WithSpinner from "../../components/with-spinner/with-spinner";
 import {
   firestore,
   convertonSnapshotToMap,
@@ -9,27 +10,42 @@ import {
 import { connect } from "react-redux";
 import { updateCollections } from "../../redux/shop/shopAction";
 
+const CollectionOverViewWithSpinner = WithSpinner(CollectionOverView);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class Shoppage extends React.Component {
+  state = {
+    isLoading: true,
+  };
   unsubscribeFromSnapshot = null;
   componentDidMount() {
     const { updateCollections } = this.props;
-    // console.log(updateCollections, "???");
     const collectionRef = firestore.collection("collections");
     this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
       async (snapshot) => {
         const collectionsMap = convertonSnapshotToMap(snapshot);
         updateCollections(collectionsMap);
+        this.setState({ isLoading: false });
       }
     );
   }
   render() {
     const { match } = this.props;
+    const { isLoading } = this.state;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionOverView} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionOverViewWithSpinner isLoading={isLoading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => (
+            <CollectionPageWithSpinner isLoading={isLoading} {...props} />
+          )}
         />
       </div>
     );
